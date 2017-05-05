16f88a5429844369530f0854ba27251b4e47a57c
Gemma <gemmawvoakes@gmail.com>
2017-05-05 12:54:30 +0100

    Pimped my quiz

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ $(document).ready(function () {
 
 
     var score = 0;
-    runningTotal = document.getElementById('Total')
-    runningTotal.innerHTML = "score: " + score;
+    runningTotal = document.getElementById('total')
+    runningTotal.innerHTML = "Score: " + score;
     $(".correct").click(function () {
         $(this).removeClass("btn-primary").addClass("btn-success").off("click");
         $(this).siblings().removeClass("btn-primary").off("click");
         score += 1;
-         $(this).parents().next('.questions').show();
-        
+        $(this).parents().next('.questions').show();
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
 
         return runningTotal.innerHTML = "Score: " + score + "/10";
     });
     $(".incorrect").click(function () {
         $(this).removeClass("btn-primary").addClass("btn-danger").off("click");
         $(this).siblings().removeClass("btn-primary").off("click");
-         $(this).parents().next('.questions').show();
-        
+        $(this).parents().next('.questions').show();
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
     });
 
 });
